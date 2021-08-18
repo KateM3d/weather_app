d416bb8a9111ea44523b0791d536e27ff81bdc99
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-18 19:55:05 -0400

    added content for the City, Country, feels Like

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const api = {
     endpoint: "http://api.openweathermap.org/data/2.5/",
-    key: ""
+    key: "9792f094c713603c46830048bc0e283e"
 }
 
 const input = document.querySelector('#input');
@@ -18,5 +18,19 @@ async function getInfo(data) {
     const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
 
     const result = await res.json();
+
+    displayResult(result);
+
+}
+
+function displayResult(result) {
     console.log(result);
+    let city = document.querySelector('#city');
+    let temperature = document.querySelector('#temperature');
+    let feelsLike = document.querySelector('#feelsLike');
+
+    city.textContent = `${result.name},${result.sys.country}`;
+    temperature.innerHTML = `${Math.round(result.main.temp)}<span>C</span>`;
+    feelsLike.innerHtml = `<span>Feels like:</span>${Math.round(result.main.feels_like)}<span>C</span>`;
+
 }
