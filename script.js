576234e6a8c642abdf1967fdb1a77d9fab5d723a
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-19 18:19:16 -0400

    added all the info but date change

--- script.js
+++ script.js
@@ -28,9 +28,12 @@ function displayResult(result) {
     let city = document.querySelector('#city');
     let temperature = document.querySelector('#temperature');
     let feelsLike = document.querySelector('#feelsLike');
+    let conditions = document.querySelector('#conditions');
+    let variation = document.querySelector('#variation');
 
     city.textContent = `${result.name},${result.sys.country}`;
     temperature.innerHTML = `${Math.round(result.main.temp)}<span>C</span>`;
-    feelsLike.innerHtml = `<span>Feels like:</span>${Math.round(result.main.feels_like)}<span>C</span>`;
-
+    feelsLike.innerHTML = `<span>Feels like:</span>${Math.round(result.main.feels_like)}<span>C</span>`;
+    conditions.textContent = `${result.weather[0].main}`;
+    variation.innerHTML = `<span>Min:</span> ${Math.round(result.main.temp_min)}<span>C</span><span>Max:</span> ${Math.round(result.main.temp_max)}<span>C</span>`;
 }
