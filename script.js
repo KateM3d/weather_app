d1a913933336fa0e91e7541467c0104bebe30bed
KateM3d <ekaterina8medvedeva@gmail.com>
2021-08-19 18:42:57 -0400

    added informatin about the date

--- script.js
+++ script.js
@@ -24,16 +24,33 @@ async function getInfo(data) {
 }
 
 function displayResult(result) {
-    console.log(result);
     let city = document.querySelector('#city');
     let temperature = document.querySelector('#temperature');
     let feelsLike = document.querySelector('#feelsLike');
     let conditions = document.querySelector('#conditions');
     let variation = document.querySelector('#variation');
 
+    getTodaysDate();
+
     city.textContent = `${result.name},${result.sys.country}`;
     temperature.innerHTML = `${Math.round(result.main.temp)}<span>C</span>`;
     feelsLike.innerHTML = `<span>Feels like:</span>${Math.round(result.main.feels_like)}<span>C</span>`;
     conditions.textContent = `${result.weather[0].main}`;
     variation.innerHTML = `<span>Min:</span> ${Math.round(result.main.temp_min)}<span>C</span><span>Max:</span> ${Math.round(result.main.temp_max)}<span>C</span>`;
+}
+
+function getTodaysDate() {
+    const myDate = new Date();
+    let showDate = document.querySelector('#date');
+
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let day = days[myDate.getDay()];
+    let todaysDate = myDate.getDate();
+    let month = months[myDate.getMonth()];
+    let year = myDate.getFullYear();
+
+
+    showDate.textContent = `${day}, ${todaysDate} ${month} ${year}`;
 }
